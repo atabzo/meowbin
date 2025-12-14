862d2c71b0c3e27fba959e6e6c14639b4074b355
Bhanu <myemail.bhanu@gmail.com>
2025-12-14 22:57:32 +0530

    figured out the header design for the time being

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,38 @@ import { useState } from 'react';
 import './App.css';
 import './fonts.css';
 
-
 function App() {
   const [text, setText] = useState('');
+  
+  return (
+    <div className="app-container">
+      <div className="header">
+            <span className="header-normal">Welcome to</span>
+            <span className="header-highlight">meowtion!</span>
+        </div>
 
-
-  return (  // ← Only ONE return!
-    <textarea
-      className="canvas"
-      value={text}
-      onChange={(e) => setText(e.target.value)}
-      placeholder="type type type"
-      spellCheck={false}  // ← This turns off the red squiggly lines
-      autoComplete="off"  // ← This turns off autocomplete suggestions
-      autoCorrect="off"   // ← This turns off auto-correction on mobile
-      autoFocus
-    />
+    <div className="app-container">
+      <div className="canvas-wrapper">
+        <textarea
+          className="canvas"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          placeholder="type type type"
+          spellCheck={false}
+          autoCorrect="off"
+          autoCapitalize="off"
+          autoComplete="off"
+          autoFocus
+        />
+      </div>
+    </div>
+    </div> 
   );
 }
 
 export default App;
+
+
+
+
+
