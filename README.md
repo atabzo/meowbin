bc46e90be2c0d540ec90dfb8e1e1f26bd8664dd6
Bhanu <myemail.bhanu@gmail.com>
2025-12-14 17:17:27 +0530

    setting up the fonts

--- README.md
+++ README.md
@@ -13,14 +13,20 @@ meowbin: a simple interactive notebin web app
 
 ## features to be added!
 
-- font formatting options
+- font formatting options (font-style, color, headings)
 - the flexibility of choosing your own custom link
 - an optional feature to protect the note with a password
 - a live wpm tracker as you type
+- ability to add images
+- exporting the page to a pdf-content
 
 </samp>
 
 
 ## screenshots!
 
+<samp> 
+
 will be added soon! :D
+
+</samp>
--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,24 @@
 import { useState } from 'react';
 import './App.css';
+import './fonts.css';
+
 
 function App() {
   const [text, setText] = useState('');
 
-  return (
+
+  return (  // ← Only ONE return!
     <textarea
-      className= "canvas"
-      value = {text}
+      className="canvas"
+      value={text}
       onChange={(e) => setText(e.target.value)}
       placeholder="type type type"
+      spellCheck={false}  // ← This turns off the red squiggly lines
+      autoComplete="off"  // ← This turns off autocomplete suggestions
+      autoCorrect="off"   // ← This turns off auto-correction on mobile
+      autoFocus
     />
-  )
+  );
 }
 
 export default App;
